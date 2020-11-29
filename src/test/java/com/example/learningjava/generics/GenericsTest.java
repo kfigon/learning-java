e5c440976dc5fd465fa959af878117d03ae27fed
Kamil <kamil.figon@gmail.com>
2020-11-29 14:23:41 +0100

    init commit

---
+++ src/test/java/com/example/learningjava/generics/GenericsTest.java
@@ -0,0 +1,12 @@
+package com.example.learningjava.generics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GenericsTest {
+    @Test
+    void test1() {
+        assertThat(true).isTrue();
+    }
+}
