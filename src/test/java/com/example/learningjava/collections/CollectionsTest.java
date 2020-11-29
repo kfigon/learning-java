7e4d9e8ff815017a16d63c137b2c224cb0af0edc
Kamil <kamil.figon@gmail.com>
2020-11-29 15:54:07 +0100

    comparable

--- src/test/java/com/example/learningjava/collections/CollectionsTest.java
+++ src/test/java/com/example/learningjava/collections/CollectionsTest.java
@@ -1,10 +1,44 @@
 package com.example.learningjava.collections;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class CollectionsTest {
+// set, list, queue/dequeue
+// maps
+
+//    usually when using things other than lists
+//    we'll need to provide comparable interface and overload equals and hash
+
+    @AllArgsConstructor
+    class Foo implements Comparable<Foo> {
+        int id;
+        @Override
+        public int compareTo(Foo o) {
+            if (this.id > o.id) {
+                return 1;
+            } else if (this.id < o.id) {
+                return -1;
+            }
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "Foo{" + id + "} ";
+        }
+    }
+
     @Test
     void test1() {
-
+        List<Foo> list = Arrays.asList(new Foo(3), new Foo(5), new Foo(123), new Foo(-1));
+        list.forEach(System.out::print);
+        Collections.sort(list);
+        System.out.println();
+        list.forEach(System.out::print);
     }
 }
