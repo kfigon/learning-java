057b58cf2da568bf68391ef79eaa0ebf39da6987
Kamil <kamil.figon@gmail.com>
2021-01-04 22:05:23 +0100

    volatile

--- src/test/java/com/example/learningjava/threading/SynchroTest.java
+++ src/test/java/com/example/learningjava/threading/SynchroTest.java
@@ -2,13 +2,20 @@ package com.example.learningjava.threading;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SynchroTest {
     AtomicInteger atomicInteger = new AtomicInteger(10);
     int regularInteger = 10;
+
+    //        volatile makes ONLY ONE atomic read OR write, where atomics can do few atomic operations, e.g getAndAdd
+    volatile int volatileInt = 0; // `synchronised` for variables. Ensures that the variable won't be cached (read the latest value from main memory), and JVM won't optimize it by rearranging
 
     @Test
     void test1() throws InterruptedException {
@@ -42,8 +49,8 @@ class SynchroTest {
         thread.start();
         thread1.start();
 //
-//        thread.join();
-//        thread1.join();
+        thread.join();
+        thread1.join();
 
         System.out.println("final res: " +regularInteger);
     }
@@ -58,12 +65,25 @@ class SynchroTest {
         Thread thread1 = new Thread(r2);
         thread.start();
         thread1.start();
-//
-//        thread.join();
-//        thread1.join();
+
+        thread.join();
+        thread1.join();
 
         System.out.println("final res: " +regularInteger);
     }
+
+    @Test
+    void volatileTest() throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(100);
+
+//        volatile makes ONLY ONE atomic read OR write, where atomics can do few atomic operations, e.g getAndAdd
+        IntStream.range(0, 10000)
+                .forEach(count -> service.submit(() -> volatileInt++));
+        service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+
+        assertThat(volatileInt).isEqualTo(10000);
+    }
+
 
 //    synchronised method
 //    sync block - DONT use local variables, use this or some member
